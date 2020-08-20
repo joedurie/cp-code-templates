5c71a30b4f80f9ae587e223ce843673590a6dceb
joedurie <45380849+joedurie@users.noreply.github.com>
2020-08-20 00:13:51 -0400

    geometry changes

--- geometry.cpp
+++ geometry.cpp
@@ -1,4 +1,5 @@
 //Geometry
+//Geometry
 #pragma GCC target ("avx2")
 #pragma GCC optimize ("O3")
 #pragma GCC optimize ("unroll-loops")
@@ -27,19 +28,19 @@ struct circ { pt C; ld R; };
 
 #define X real()
 #define Y imag()
-#define SQ(x) ((x) * (x))				//square of x
-#define CRS(a, b) (conj(a) * (b)).Y		//scalar cross product
-#define DOT(a, b) (conj(a) * (b)).X		//dot product
-#define U(p) ((p) / abs(p))				//unit vector in direction of p
-#define Z(p) (abs(p) < EPS)				//true if p approx. (0, 0)
+#define SQ(x) ((x) * (x)) //square of x
+#define CRS(a, b) (conj(a) * (b)).Y	//scalar cross product
+#define DOT(a, b) (conj(a) * (b)).X	//dot product
+#define U(p) ((p) / abs(p))	//unit vector in direction of p (DON'T USE ON ZERO VECTOR)
+#define Z(p) (abs(p) < EPS)	//true if p approx. (0, 0)
 
 //constants (INF and EPS may need to be modified)
-const ld PI = acos(-1), INF = 1e30, EPS = 0.0001;
-const pt I = {0, 1}, INV = {INF, INF};
+constexpr ld PI = acos(-1), INF = 1e30, EPS = 0.0001;
+constexpr pt I = {0, 1}, INV = {INF, INF};
 
 //less than operator for pts
 constexpr bool operator<(const pt a, const pt b) {
-	return a.X == b.X ? a.Y < b.Y : a.X < b.X;
+	return Z(a.X - b.X) ? a.Y < b.Y : a.X < b.X;
 }
 
 //comparator for sorting pts
@@ -56,10 +57,10 @@ line s2p(pt p, pt q) { return {p, q - p, 1}; }
 //line through p with angle th
 line ang_line(pt p, ld th) { return {p, polar((ld)1, th), 0}; }
 
-//true if d1 and d2 parallel
+//true if d1 and d2 parallel (zero vectors considered parallel to everything)
 bool parallel(pt d1, pt d2) { return Z(d1) || Z(d2) || Z(CRS(U(d1), U(d2))); }
 
-//"above" may not be in usual sense, but consistent for fixed l
+//"above" here means if l & p are rotated such that l.D points in the +x direction, then p is above l
 bool above_line(pt p, line l) { return CRS(p - l.P, l.D) > 0; }
 
 //true if p is on line l
@@ -74,8 +75,8 @@ pt intsct(line l1, line l2) {
 
 //closest pt on l to p
 pt cl_pt_on_l(pt p, line l) {
-	pt q = intsct(l, {p, l.D * I, 0});
-	if(!Z(q - INV)) return q;
+	pt q = l.P + DOT(l.D, p - l.P) * U(l.D);
+	if(!on_line(q, l)) return q;
 	return abs(p - l.P) < abs(p - l.P - l.D) ? l.P : l.P + l.D;
 }
 
@@ -117,10 +118,10 @@ circ circumcirc(pt a, pt b, pt c) {
 }
 
 //convex hull of pts (O(n) construction)
-pair<vector<pt>, vector<pt>> get_hull(vector<pt> pts) {
+pair<vector<pt>, vector<pt>> get_hull(vector<pt>& pts) {
 	vector<pt> hu, hd;
 	sort(pts.begin(), pts.end(), p_cmp);
-	#define DO_HULL(h) for(auto p : pts) { \
+	#define DO_HULL(h) for(pt p : pts) { \
 		while(h.size() >= 2 && CRS(h.back() - h[h.size() - 2], p - h[h.size() - 2]) <= 0) h.pop_back(); \
 		h.push_back(p); }
 	DO_HULL(hu)
@@ -130,43 +131,57 @@ pair<vector<pt>, vector<pt>> get_hull(vector<pt> pts) {
 }
 
 //is pt p on the (upper or lower) convex hull given by pts
-bool on_hull(vector<pt> pts, pt p) {
+bool on_hull(vector<pt>& pts, pt p) {
 	if(p < *pts.begin() || *pts.rbegin() < p) return true;
 	auto q = --upper_bound(pts.begin(), pts.end(), p, p_cmp);
 	return CRS(p - *q, *++q - p) > 0;
 }
 
+//is pt p inside the (not necessarily convex) polygon given by poly
+bool in_poly(pt p, vector<pt>& poly) {
+	line l = {p, {INF, INF * PI}, 1};
+	bool ans = false;
+	pt lst = poly.back();
+	for(pt q : poly) {
+		line s = s2p(q, lst); lst = q;
+		if(on_line(p, s)) return true; //change if border not included
+		else if(!Z(intsct(l, s) - INV)) ans = !ans;
+	}
+	return ans;
+}
+
 //area of polygon, vertices in order (cw or ccw)
-ld area(vector<pt> poly) {
-	ld ans = CRS(poly.back(), poly[0]);
-	for(ll i = 1; i < poly.size(); i++) ans += CRS(poly[i - 1], poly[i]);
-	return abs(ans / 2);
+ld area(vector<pt>& poly) {
+	ld ans = 0;
+    pt lst = poly.back();
+    for(pt p : poly) ans += CRS(lst, p), lst = p;
+    return abs(ans / 2);
 }
 
 //perimeter of polygon, vertices in order (cw or ccw)
-ld perim(vector<pt> poly) {
-	ld ans = abs(poly.back() - poly[0]);
-	for(ll i = 1; i < poly.size(); i++) ans += abs(poly[i] - poly[i - 1]);
+ld perim(vector<pt>& poly) {
+	ld ans = 0;
+    pt lst = poly.back();
+    for(pt p : poly) ans += abs(lst - p), lst = p;
 	return ans;
 }
 
 //centroid of polygon, vertices in order (cw or ccw)
-pt centroid(vector<pt> poly) {
-	pt cent = {0, 0}, p1 = poly.back();
-	ld pArea = 0;
-	for(ll i = 0; i < poly.size() - 1; i++) {
-		pt p2 = poly[i], p3 = poly[(i + 1) % (poly.size() - 1)];
-		ld tArea = CRS(p1, p2) + CRS(p2, p3) + CRS(p3, p1);
-		cent += tArea * (p1 + p2 + p3) / (ld)3;
-		pArea += tArea;
-	}
-	return cent / pArea;
+pt centroid(vector<pt>& poly) {
+    ld area = 0;
+    pt lst = poly.back(), ans = {0, 0};
+    for(pt p : poly) {
+        area += CRS(lst, p);
+        ans += CRS(lst, p) * (lst + p) / (ld)3;
+		lst = p;
+    }
+    return ans / area;
 }
 
 //vector of intersection pts of two circs (up to 2)
 vector<pt> intsctCC(circ c1, circ c2) {
 	ld d = abs(c1.C - c2.C);
-	if(d > c1.R + c2.R || d < abs(c1.R - c2.R)) return {INV, INV};
+	if(d > c1.R + c2.R || d < abs(c1.R - c2.R) || Z(d)) return {INV, INV};
 	ld h = (SQ(d) - SQ(c2.R) + SQ(c1.R)) / (2 * d);
 	pt v = U(I * (c2.C - c1.C)) * sqrt(SQ(d) - SQ(h));
 	pt p = c1.C + U(c2.C - c1.C) * h;
@@ -178,11 +193,25 @@ vector<pt> intsctCC(circ c1, circ c2) {
 //vector of intersection pts of a line and a circ (up to 2)
 vector<pt> intsctCL(circ c, line l) {
 	if(dist_to(c.C, l) > c.R) return {};
-	pt p = cl_pt_on_l(c.C, l);
+	pt p = cl_pt_on_l(c.C, l.s(0));
 	pt v = U(l.D) * sqrt(SQ(c.R) - norm(p - c.C));
 	vector<pt> ans;
 	if(on_line(p + v, l)) ans.push_back(p + v);
 	if(on_line(p - v, l) && !Z(v)) ans.push_back(p - v);
+	return ans;
+}
+
+//external tangents of two circles (negate c2.R for internal tangents)
+vector<line> circTangents(circ c1, circ c2) {
+	pt d = c2.C - c1.C;
+	ld dr = c1.R - c2.R, d2 = norm(d), h2 = d2 - SQ(dr);
+	if(Z(d2) || h2 < 0) return {};
+	vector<line> ans;
+	for(ld sg : {-1, 1}) {
+		pt v = (d * dr + d * I * sqrt(h2) * sg) / d2;
+		ans.push_back(s2p(c1.C + v * c1.R, c2.C + v * c2.R));
+	}
+	if(Z(h2)) ans.pop_back();
 	return ans;
 }
 
@@ -191,12 +220,3 @@ int main() {
     cin.tie(0);
 	
 }
-
-/* TODO:
-fix on_hull
-make all sets/vectors pass by reference
-common tangents of circles
-streaming convex hull
-golf everything (a lot)
-lots of testing
-*/
