e0b201f9f71746c937b490225214846b979fc641
joedurie <45380849+joedurie@users.noreply.github.com>
2020-09-01 02:04:20 -0400

    golfing cht.cpp

--- cht.cpp
+++ cht.cpp
@@ -11,28 +11,27 @@ typedef pair<ll, ll> pl;
 
 #define G(x) ll x; cin >> x;
 #define F(i, l, r) for(ll i = l; i < r; i++)
+#define A(x) (x).begin(), (x).end()
 #define N 100010
-#define INF 10000000000000ll
+#define INF 1000000000000000000ll
 
 struct seg { ld x; ll m, b; };
-struct s_cmp { bool operator() (const seg& s, const seg& t) const { return s.x < t.x; } };
 
-set<seg, s_cmp> hull;
+vector<seg> hull;
 
 void insert(ll m, ll b) { //lines inserted in inc. slope order for maxhull, desc. for minhull
 	while(hull.size()) {
-		seg s = *hull.rbegin();
-		if(s.b + s.m * s.x <= b + m * s.x) hull.erase(s); //>= for minhull
-		else break;
+		seg s = hull.back();
+		if(s.b + s.m * s.x > b + m * s.x) { //< for minhull
+			if(s.m - m) hull.push_back({(b - s.b) / (ld)(s.m - m), m, b});
+			return;
+		}
+		hull.pop_back();
 	}
-	if(hull.empty()) { hull.insert({-INF, m, b}); return; }
-	seg s = *hull.rbegin();
-	if(s.m == m) return;
-	ld x = (b - s.b) / (ld)(s.m - m);
-	hull.insert({x, m, b});
+	hull.push_back({-INF, m, b});
 }
 
 ll query(ll x) {
-	seg s = *--hull.upper_bound({(ld)x, INF, INF});
+	seg s = *--upper_bound(A(hull), (seg){(ld)x, INF, INF}, [](const seg s1, const seg s2) { return s1.x < s2.x; });
 	return s.b + s.m * x;
 }
