361e90b5cc1937f56a115dafa83cfa5ef284be7c
Joe Durie <joedurie17@gmail.com>
2023-01-17 20:52:08 -0500

    removing trd + fixing intsctCC

--- gnyrtopics/geometry.cpp
+++ gnyrtopics/geometry.cpp
@@ -244,14 +244,10 @@ pt centroid(vector<pt>& poly) {
 //vector of intersection pts of two circs (up to 2) (if circles same, returns empty vector)
 vector<pt> intsctCC(circ c1, circ c2) {
     pt d = c2.C - c1.C;
-    ld d2 = norm(d);
-    if(Z(d)) return {}; //concentric
-    ld pd = (d2 + c1.R * c1.R - c2.R * c2.R) / 2;
-    ld h2 = c1.R * c1.R - pd * pd / d2;
-    if(h2 < 0) return {};
-    pt p = c1.C + d * pd / d2, h = d * I * sqrtl(h2 / d2);
-    if(Z(h)) return {p};
-    return {p - h, p + h};
+	if(Z(d) || abs(d) >= c1.R + c2.R - EPS) return {};
+	cout << (c1.R * c1.R + norm(d) - c2.R * c2.R) / (2 * c1.R * abs(d)) << '\n';
+	ld th2 = acosl((c1.R * c1.R + norm(d) - c2.R * c2.R) / (2 * c1.R * abs(d)));
+	return {c1.C + polar(c1.R, arg(d) + th2), c1.C + polar(c1.R, arg(d) - th2)};
 }
 
 //vector of intersection pts of a line and a circ (up to 2)
